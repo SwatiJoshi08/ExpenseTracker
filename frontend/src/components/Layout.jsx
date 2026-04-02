4ac7a07872c878403e21ebf09c7753c2bee485aa
Vibhanshu Joshi <vibhanshujoshi26@gmail.com>
2026-04-02 10:46:16 +0530

    second commit

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { styles } from "../assets/dummyStyles.js";
 import Navbar from "./Navbar";
+import { Sidebar } from "lucide-react";
 
 const Layout = ({ onLogout, user }) => {
+  const [SidebarCollapsed, setSidebarCollapsed] = useState(false);
   return (
     <div className={styles.layout.root}>
       <Navbar user={user} onLogout={onLogout} />
+      <Sidebar
+        user={user}
+        isCollapsed={setSidebarCollapsed}
+        setSidebarCollapsed={setSidebarCollapsed}
+      />
     </div>
   );
 };
---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { sidebarStyles, cn } from "../assets/dummyStyles";
+const Sidebar = (user, isCollapsed, setIs) => {
+  return <div></div>;
+};
+
+export default Sidebar;
